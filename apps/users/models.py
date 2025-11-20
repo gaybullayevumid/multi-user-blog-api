abeea83fc5333119ad5ca286a999872b4a71048e
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-11-20 21:32:27 +0500

    Add initial user app structure and configuration

---
+++ apps/users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+
+
+    def __str__(self):
+        return self.username
