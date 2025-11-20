736726cf28516015301db96b384c0ec683113ab0
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-11-20 21:54:17 +0500

    feat: add auth and profile endpoints to URLs

---
+++ apps/users/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import RegisterView, ProfileView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenBlacklistView,
+)
+
+urlpatterns = [
+    path('signup/', RegisterView.as_view(), name='signup'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('logout/', TokenBlacklistView.as_view(), name='logout'),
+    path('profile/', ProfileView.as_view(), name='profile')
+]
