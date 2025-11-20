c0678feb5562a4f7d832f647912edd0636c9383e
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-11-20 21:49:08 +0500

    feat: add ProfileView with self-update permission

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
-from .serializers import RegisterSerializer, UserSerializer
 from django.contrib.auth import get_user_model
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import RegisterSerializer, UserSerializer
+from .permissions import IsSelf
 
 # Create your views here.
 
@@ -11,3 +14,12 @@ class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     queryset = User.objects.all()
 
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated, IsSelf]
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+    def get_object(self):
+        return self.request.user
