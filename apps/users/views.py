e9add1eac284df32aea5d42c078398b4882da349
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-11-20 21:45:40 +0500

    feat: add RegisterView for user signup

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import RegisterSerializer, UserSerializer
+from django.contrib.auth import get_user_model
 
 # Create your views here.
+
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    queryset = User.objects.all()
+
