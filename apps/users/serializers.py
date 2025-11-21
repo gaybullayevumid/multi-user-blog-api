221a335d08d92fc591d64cf8ba17357406193777
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-11-21 22:09:45 +0500

    feat: add password confirmation and validation logic to RegisterSerializer

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -8,14 +8,21 @@ User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, requered=True)
 
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'password2']
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({"password": "Password do not match"})
+        return data
 
 
     def create(self, validated_data):
+        validated_data.pop('password2')
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
