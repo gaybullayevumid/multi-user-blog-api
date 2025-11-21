02a4b130335bd49695d4bcf49833e60cc770bf1b
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-11-21 22:03:31 +0500

    feat: add unique email field and update REQUIRED_FIELDS

--- apps/users/models.py
+++ apps/users/models.py
@@ -5,8 +5,13 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
+
+
+    REQUIRED_FIELDS = ['email']
+    USERNAME_FIELD = 'username'
 
 
     def __str__(self):
