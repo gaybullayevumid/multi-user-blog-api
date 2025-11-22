912cd9fd5cbc76bdbae07f6a4a16e8a650f3bac0
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-11-22 11:14:56 +0500

    feat: create Post model linked to User

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
