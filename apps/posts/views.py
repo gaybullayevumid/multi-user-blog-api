9b71efc020f5d7e2ffce07b8feb3ec483201c00a
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-11-22 11:47:22 +0500

    feat: add PostListCreateView and PostDetailView

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .serializers import PostSerializer
+from .models import Post
 
 # Create your views here.
+
+
+class PostListCreateView(generics.ListCreateAPIView):
+    queryset = Post.objects.all().order_by("-created_at")
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
