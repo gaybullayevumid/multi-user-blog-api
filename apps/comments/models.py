df3612ec051a6dc73218a7d3d65e9f3d813bbf3d
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-11-24 11:15:15 +0500

    feat: create Comment model linked to Post and User

--- apps/comments/models.py
+++ apps/comments/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+from posts.models import Post
 
 # Create your models here.
+
+
+class Comments(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateField(auto_now_add=False)
+    updated_at = models.DateField(auto_now=False)
+
+
+    def __str__(self):
+        return f"{self.author.username} -> {self.post.title[:20]}"
+
