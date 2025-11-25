bf3844bc87d034fb06492d7e270f6b43f4251850
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-11-25 12:06:21 +0500

    feat: add comment URLs to main API router

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("apps.users.urls")),
     path('api/', include("apps.posts.urls")),
+    path('api/', include("apps.comments.urls")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
