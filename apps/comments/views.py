30afe875190e617c33198030bacd051a42ed8d68
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-11-25 11:50:16 +0500

    feat: add comments feature with CRUD, permissions, and serializers

--- apps/comments/views.py
+++ apps/comments/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Comments
+from .serailizers import CommentSerializer
+from .permissions import IsCommentAuthorOrReadOnly
+
 
 # Create your views here.
+
+class CommentListCreateView(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        post_id =  self.kwargs['post_id']
+        return Comment.objects.filter(post_id=post_id).order_by('-created_at')
+
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs['post_id']
+        serializer.save(author=self.request.user, post_id=post_id)
+
+
+class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comments.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsCommentAuthorOrReadOnly]
