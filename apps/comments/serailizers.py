b29c0ff80d921fe318b014626e7c79b35a9fc734
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-11-25 11:32:27 +0500

    feat: add CommentSerializer with author and post read-only fields

---
+++ apps/comments/serailizers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Comments
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(write_only=True)
+
+
+    class Meta:
+        model = Comments
+        fields = ['id', 'post', 'author', 'content', 'created_at', 'updated_at']
+        read_only_fields = ['author', 'post']
