7d8ef4089e109af12a07ed7fa050d74726ef8dea
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-11-26 20:09:51 +0500

    feat: add LikeSerializer for like model

--- apps/likes/views.py
+++ apps/likes/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Like
+from apps.posts.models import Post
 
 # Create your views here.
+
+
+class ToggleLikeView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, post_id):
+        post = Post.objects.get(id=post_id)
+        user = request.user
+        
+        like = Like.objects.filter(user=user, post=post).first()
+
+        if like:
+            like.delete()
+            return Response({"message": "Like removed"}, status=status.HTTP_200_OK)
+        
+        Like.objects.create(user=user, post=post)
+        return Response({"message": "Post Liked"}, status=status.HTTP_201_CREATED)
