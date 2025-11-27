9c7637e441e852f9075fc4cde451abfa254d1714
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-11-27 21:25:42 +0500

    feat: add Redoc documentation view to API URLs

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -13,5 +17,8 @@ urlpatterns = [
         "api/docs/swagger/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
+    ),
+    path(
+        "api/docs/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
     ),
 ]
